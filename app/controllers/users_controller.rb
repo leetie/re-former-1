839e65a6861b7337af507fd42fa5a3a72117486f
Jesse Parsons <savvyjesse@aol.com>
2020-08-26 14:36:48 -0400

    add show page and show action to userscontroller, add show to routes, minor change to users/edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def new
-        @user = User.new
     end
 
     def create
-       # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
          @user = User.new(user_params)
 
         if @user.save
@@ -28,7 +27,14 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+    end
+
     private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
